45bf02fe6b8405bef8048b3df37e918b948a822f
unknown <kirkster96@gmail.com>
2020-10-10 22:39:00 -0700

    assignment 1 complete

---
+++ Assignment1/asng1_t02/Romi-NoRpi-Debug/Romi-NoRpi-Debug.ino
@@ -0,0 +1,52 @@
+#include <Romi32U4.h>
+#include <PololuRPiSlave.h>
+
+Romi32U4Motors motors;
+Romi32U4Encoders encoders;
+Romi32U4ButtonA buttonA;
+
+void setup() {
+  Serial.begin(57600);
+
+  // put your setup code here, to run once:
+  ledYellow(false);
+  ledGreen(true);
+  ledRed(false);
+}
+
+float _debug_linear_ms = 0.05;
+float _debug_angle_rs = 0.0;
+bool turnTime = true;
+int counter = 0;
+void _DEBUG_PID_CONTROL() {
+  static float _linear_ms_change = 0.1;
+  set_twist_target(_debug_linear_ms, _debug_angle_rs);
+}
+
+void loop() {
+  set_twist_target(_debug_linear_ms, _debug_angle_rs);
+ 
+  // put your main code here, to run repeatedly:
+  if (everyNmillisec(10)) {
+      // ODOMETRY
+      //calculateOdom();
+      doPID();
+      counter++;
+      if(counter > 200)
+      {
+        counter=0;
+        if(turnTime){
+          _debug_linear_ms=0.0;
+          _debug_angle_rs=0.25;
+          turnTime=!turnTime;
+        }
+        else
+        {
+          _debug_linear_ms=0.05;
+          _debug_angle_rs=0.0;
+          turnTime=!turnTime;
+          }
+      }
+    }
+
+}
